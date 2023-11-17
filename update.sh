b557524a30981210a94f3b628bad439592ae5bf7
Erhan Bagdemir <erhan.bagdemir@gmail.com>
2023-11-17 23:07:47 +0100

    Updated update script.

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash --login
 
+source /usr/share/rvm/scripts/rvm
+rvm use 3.0.0
 cd ~/blogs/blog.bagdemir.com.2023 && git pull origin main 
-cd ~/blogs/blog.bagdemir.com.2023 && jekyll build
+cd ~/blogs/blog.bagdemir.com.2023 && bundle && jekyll build
